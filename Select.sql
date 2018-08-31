b99bf980e7734768a4de307d064138f12dffc9f1
exuberanteasy <exuberanteasy@gmail.com>
2018-08-31 19:56:22 +0800

    Update Select.sql

--- Select.sql
+++ Select.sql
@@ -5,3 +5,5 @@ Select 姓名 As 名字, English as 外文, 中文 from 成績單
 select 姓名, 中文 + 數學 + English as 期中考總分 From 成績單
 
 Select 姓名, 數學 From 成績單 Where 中文 > 90
+
+select * from 成績單 Where 姓名 Like '林 %' --# 查不到 ??
