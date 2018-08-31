22bbfc05d23c167b515c83678457c53991f37041
exuberanteasy <exuberanteasy@gmail.com>
2018-08-31 15:13:43 +0800

    Update System.IO.cs

--- System.IO.cs
+++ System.IO.cs
@@ -6,7 +6,7 @@
 //=== 取得電腦上的邏輯磁碟名稱
 foreach(string Name in Directory.GetLogicalDrives())
     
-//=== 
+//=== 取得參數 path 指定的資料夾內所有檔案的名稱(包含完整路徑)
 foreach (string Name in Directory.GetFiles(@"C:\windows", "b*"))
     
 //=== 取得參數 path 指定的資料夾
