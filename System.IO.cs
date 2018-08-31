b05c73da64fdf34c0cdde080049674c5a46e0a91
exuberanteasy <exuberanteasy@gmail.com>
2018-08-31 15:07:45 +0800

    Update System.IO.cs

--- System.IO.cs
+++ System.IO.cs
@@ -1,3 +1,7 @@
+
+//
+foreach (string Name in Directory.GetFiles(@"C:\windows", "b*"))
+//
 foreach(string Name in Directory.GetFileSystemEntries(@"C:\windows", "b*"))
 {
     Console.WriteLine(Name);
