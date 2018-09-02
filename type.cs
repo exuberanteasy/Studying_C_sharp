dea3740ed595f50ce27a51e7e092a41377c65bc7
exuberanteasy <exuberanteasy@gmail.com>
2018-09-03 05:30:05 +0800

    Create type.cs

---
+++ type.cs
@@ -0,0 +1,29 @@
+type(型別)
+
+value type
+
+reference type
+
+整數型別
+
+floating point(浮點數型別)
+
+decimal
+
+bool
+
+char
+
+string
+
+object
+
+型別的結構
+
+type conversion
+implicit conversion
+explicit conversion
+
+// 先把輪廓寫出來
+// 刻意低調
+// 刻意愉快
