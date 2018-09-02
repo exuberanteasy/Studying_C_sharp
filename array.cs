b3bf2827f7bf6abeb008aa7608bcb5e2cd1aeb2a
exuberanteasy <exuberanteasy@gmail.com>
2018-09-03 06:15:38 +0800

    Update array.cs

--- array.cs
+++ array.cs
@@ -14,11 +14,10 @@ jagged array(不規則陣列)
 
 //=================================================
 System.Array 類別
+
 Public Property(公有屬性)
-
-IsFixedSize  //取得表示陣列是否為固定大小的布林值，通常為 true
-
-IsReadOnly  //取得表示陣列是否為唯讀的布林值，通常為 false
+╠ IsFixedSize  //取得表示陣列是否為固定大小的布林值，通常為 true
+╠ IsReadOnly  //取得表示陣列是否為唯讀的布林值，通常為 false
 
 Public Method(公有方法)
-  BinarySearch(arr,obj)  //使用二元搜尋法在 arr 陣列內搜尋 obj 所在位置的索引，傳回值為 int 型別。
+╠ BinarySearch(arr,obj)  //使用二元搜尋法在 arr 陣列內搜尋 obj 所在位置的索引，傳回值為 int 型別。
