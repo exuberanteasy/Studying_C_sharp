f4b7554b93ade0f0a615d7371691de35b3be3177
exuberanteasy <exuberanteasy@gmail.com>
2018-09-03 06:08:44 +0800

    Update array.cs

--- array.cs
+++ array.cs
@@ -11,3 +11,12 @@ one-dimension、one-rank(一維)
 multi-dimension、multi-rank(多維)
 
 jagged array(不規則陣列)
+
+//=================================================
+System.Array 類別
+Public Property(公有屬性)
+
+IsFixedSize  //取得表示陣列是否為固定大小的布林值，通常為 true
+
+IsReadOnly  //取得表示陣列是否為唯讀的布林值，通常為 false
+
