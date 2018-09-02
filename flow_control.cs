a6e55379a209d2b57335b7d03f439322ef881053
exuberanteasy <exuberanteasy@gmail.com>
2018-09-03 05:58:48 +0800

    Update flow_control.cs

--- flow_control.cs
+++ flow_control.cs
@@ -12,3 +12,8 @@ foreach(陣列迴圈)
 string[] Names = new string[] { "蕭峰", "虛竹", "段譽" };
 foreach (string Str in Names)
    Console.WriteLine(Str);
+
+//
+while
+   Console.WriteLine("請輸入「快樂」的英文");
+string Answer = 
