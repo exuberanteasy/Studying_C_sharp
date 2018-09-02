55bc36bb2e721e6025c09b37b3f4dfc8a77780ee
exuberanteasy <exuberanteasy@gmail.com>
2018-09-03 05:36:04 +0800

    Update type.cs

--- type.cs
+++ type.cs
@@ -24,6 +24,47 @@ type conversion
 implicit conversion
 explicit conversion
 
+
+取得型別的方法
+
+轉換型別的方法
+
+variable(變數)
+  name
+  value
+
+local variable
+
+member variable
+
+
+變數的宣告方式
+scope
+
+lifetime
+access level
+
+
+implicit typing(隱含型別)
+  // 隱含型別雖然便利，卻會降低程式的可讀性，因此，除非必要，盡量少用!
+  
+  scope
+  block scope
+  
+  procedure scope
+  
+  class scope
+  
+    namespace scope
+
+
+
+
+
+
+
+
+
 // 先把輪廓寫出來
 // 刻意低調
 // 刻意愉快
