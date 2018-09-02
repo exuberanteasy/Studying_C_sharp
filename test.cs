7ea864889e3a653eee07baa46aa234c66b2c0cfd
exuberanteasy <exuberanteasy@gmail.com>
2018-09-02 11:41:45 +0800

    Update test.cs

--- test.cs
+++ test.cs
@@ -1,7 +1,7 @@
 private void button1_Click(object sender, EventArgs e)
-        {
-            const double PI = 3.14;
-            double Radius = System.Convert.ToDouble(textBox1.Text);
-            double CirleArea = PI * Radius * Radius;
-            MessageBox.Show("圓的面積為" + Convert.ToString(CirleArea)); //System. 可以簡化
-        }
+{
+    const double PI = 3.14;
+    double Radius = System.Convert.ToDouble(textBox1.Text);
+    double CirleArea = PI * Radius * Radius;
+    MessageBox.Show("圓的面積為" + Convert.ToString(CirleArea)); //System. 可以簡化
+}
