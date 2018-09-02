0e4b830d6805efe372b6d17b03f152dbc0139875
exuberanteasy <exuberanteasy@gmail.com>
2018-09-03 06:31:30 +0800

    Create method.cs

---
+++ method.cs
@@ -0,0 +1,24 @@
+procedure
+
+method
+
+function
+
+subrotine
+
+general procedure
+
+reusability
+readabikity
+
+
+parameter
+
+argument
+
+//==========================================================================
+宣告方法
+
+instance
+
+static
