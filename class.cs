1ccdab813ef07da24f20e765bb964a95252d3221
exuberanteasy <exuberanteasy@gmail.com>
2018-09-03 15:57:22 +0800

    Update class.cs

--- class.cs
+++ class.cs
@@ -1,5 +1,34 @@
-class
+field
+
+property
+
+method
+
+constructor
+
+destructor
+
+indexer
+
+operator
+
+delegate
+
+event
+
+nested type
+
+
+//=============================================
+
+modifiers
+
+
+
+//=============================================
 
 instance variable
 
 static variable
+
+constant
