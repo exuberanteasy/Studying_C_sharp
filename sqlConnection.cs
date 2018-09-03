5be90f81b018e10dc74d7e94e94aec2b3ad06d0b
exuberanteasy <exuberanteasy@gmail.com>
2018-09-03 11:01:15 +0800

    Update sqlConnection.cs

--- sqlConnection.cs
+++ sqlConnection.cs
@@ -1,11 +1,11 @@
 private void Form1_Load(object sender, EventArgs e)
 {
-    sqlConnection1.Open();  //開
+    sqlConnection1.Open();  //開啟資料連接
     SqlDataReader sqlDataReader1 = sqlCommand1.ExecuteReader(); // 建立 SqlDataReader 物件
-    while (sqlDataReader1.Read())
+    while (sqlDataReader1.Read())  // 顯示查詢結果中的學生姓名
     {
         comboBox1.Items.Add(sqlDataReader1.GetString(1));
     }
-    sqlDataReader1.Close();  //關閉Sql
-    sqlConnection1.Close();
+    sqlDataReader1.Close();  //關閉 SqlDataReader 物件
+    sqlConnection1.Close();  //關閉資料連接
 }
