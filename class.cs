f674113aee361a607f4b51b438281c388fd3945f
exuberanteasy <exuberanteasy@gmail.com>
2018-09-03 16:39:25 +0800

    Update class.cs

--- class.cs
+++ class.cs
@@ -55,3 +55,24 @@ namespace TestProj_1
         }
     }
 }
+
+//=========================================================================
+namespace TestProj_2
+{
+    class Circle
+    {
+        public int radius;                   //宣告一個案例變數
+        public const float PI = 3.14F;       //宣告一個常數
+        public static int count;             //宣告一個靜態變數
+    }
+   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Circle.PI);    //透過類別的名稱顯示常數 PI 的值
+            Console.WriteLine(Circle.count); //透過類別的名稱顯示靜態變數 count 的值
+            Console.ReadLine();
+        }
+    }
+}
