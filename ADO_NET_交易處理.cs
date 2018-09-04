0caee0410a9f9cf16640c527f913540c3723f00b
exuberanteasy <exuberanteasy@gmail.com>
2018-09-05 05:40:41 +0800

    Update ADO_NET_交易處理.cs

--- ADO_NET_交易處理.cs
+++ ADO_NET_交易處理.cs
@@ -3,8 +3,18 @@ ADO.NET 交易處理
 
 Step 1:建立名為 cn 的 Connection 資料來源連接物件 => 呼叫該物件的 Begin Transaction方法
       以便取得名為 tran 的 Transaction 交易物件
-SqlConnection cn = new SqlConnection("連接字串");
-cn.Open();
-SqlTransaction tran = cn.BeginTransaction(); //傳回 tran 交易物件
+  SqlConnection cn = new SqlConnection("連接字串");
+  cn.Open();
+  SqlTransaction tran = cn.BeginTransaction(); //傳回 tran 交易物件
 
-Step 2:建立一連串要執行的資料庫操作工作的 Command 物件，並指定該 Command 物件所要執行的SQL語法、
+Step 2:建立一連串要執行的資料庫操作工作的 Command 物件，並指定該 Command 物件所要執行的 SQL語法、
+       cn 連接物件(Connection)及 tran 交易物件。
+  SqlCommand cmd1 = new SqlCommand("SQL語法 1", cn, tran);
+  SqlCommand cmd2 = new SqlCommand("SQL語法 2", cn, tran);
+
+  SqlCommand cmdN = new SqlCommand("SQL語法 N", cn, tran);
+
+Step 3:使用 Command 物件的 ExecuteNonQuery方法執行 SQL語法，使多個資料庫的操作功能能執行。
+  cmd1.ExecuteNonQuery();
+  cmd2.ExecuteNonQuery();
+  cmdN.ExecuteNonQuery();
